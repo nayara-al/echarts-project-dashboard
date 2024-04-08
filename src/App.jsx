d9a38d9c01b26344d64476cf7e94bc848195b897
Anna Nayara <anna_nayara@hotmail.com>
2024-04-08 15:04:02 -0300

    mostrando gráficos no app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import GraficoBarra from "./components/GraficoBarra";
+import GraficoDonut from  "./components/DonutChart"
+import GraficoLinha from "./components/LineChart"
+function App() {
+  return (
+    <>
+      <GraficoBarra />
+      <GraficoDonut />
+      <GraficoLinha/>
+    </>
+  );
+}
+
+export default App;
