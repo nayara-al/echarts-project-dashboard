6590395e1229faec2412d6b9eb6e0b7b84c63ab9
Anna Nayara <anna_nayara@hotmail.com>
2024-04-08 15:02:54 -0300

    gráfico donut/pie

---
+++ src/components/DonutChart/index.tsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useRef } from "react";
+import * as echarts from "echarts";
+
+export default function GraficoDonut() {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/receitaTrimestral");
+        const data = await response.json();
+        renderChart(data);
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+      }
+    };
+
+    fetchData();
+  }, [chartRef]);
+
+  const renderChart = (data) => {
+    if (!chartRef.current || !data) return;
+
+    const chart = echarts.init(chartRef.current, {
+      renderer: "svg",
+    });
+
+    const bluePalette = ["#2196F3", "#1976D2", "#0D47A1", "#1565C0"];
+
+    const options = {
+      legend: {
+        top: "10%",
+        left: "center",
+        textStyle: {
+            color: "#fff",
+        }
+      },
+      title: {
+        text: "Receita Trimestral",
+        subtext: "Distribuição por Trimestre",
+        left: "center",
+      },
+      tooltip: {
+        trigger: "item",
+        formatter: "{a} <br/>{b}: R${c} ({d}%)",
+      },
+      series: [
+        {
+          name: "Receita por Trimestre",
+          type: "pie",
+          radius: ["30%", "60%"],
+          avoidLabelOverlap: false,
+          label: {
+            show: false,
+            formatter: "{b}: {d}%\nR${c}",
+            position: "center",
+            textStyle: {
+              fontWeight: "bold",
+            },
+          },
+          emphasis: {
+            label: {
+              show: true,
+              fontSize: "20",
+              fontWeight: "bold",
+            },
+          },
+          labelLine: {
+            show: true,
+            length: 20,
+            length2: 20,
+          },
+          data: data.map((item, index) => ({
+            value: item.receita,
+            name: `Trimestre ${item.trimestre}`,
+            itemStyle: {
+                color: bluePalette[index % bluePalette.length],
+              },
+          })),
+        },
+      ],
+    };
+
+    chart.setOption(options);
+  };
+
+  return (
+    <>
+      <h1>Gráfico Donut</h1>
+      <div ref={chartRef} style={{ width: "100%", height: "500px" }}></div>;
+    </>
+  );
+}
